d58ccc6e96d984030c264a1cc15cb84cbdc8251f
Hancel Lin <imlinhanchao@foxmail.com>
2022-10-24 15:58:15 +0800

    feat: 加入点击时才载入图片

--- extension/index.js
+++ extension/index.js
@@ -8,7 +8,7 @@ function hooks() {
     return {
         async messageEvent(msg) {
             if (msg.type == 'msg') {
-                msg.data.content = msg.data.content.replace(/<span class="kaibai">([^<]*?)<\/span>/g, '<details><summary>$1</summary><img data-action="preview" src="https://sexy.1433.top/$1" class="complate_span" title="神秘代码" alt="你来晚了"/></detail>')
+                msg.data.content = msg.data.content.replace(/<span class="kaibai">([^<]*?)<\/span>/g, `<details><summary onclick="this.parentNode.querySelector('img').src='https://sexy.1433.top/$1'">$1</summary><img data-action="preview" class="complate_span" title="神秘代码" alt="你来晚了"/></detail>`)
             }
             return msg;
         }
