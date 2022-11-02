3bfb48e91ea7c16f872069defc4501fa0e0e563f
lin.hanchao <lin.hanchao@byd.com>
2022-11-02 14:32:46 +0800

    添加设置界面与CD KEY设置支援

---
+++ renderer/views/HomeView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="layout">
+    <p><input class="pretty" type="checkbox" id="filter_tail" v-model="setting.filterTail"/><label for="filter_tail">开启解析小飞棍</label></p>
+    <p><input class="pretty" type="checkbox" id="add_tail" v-model="setting.addTail"/><label for="add_tail">点击时解析小飞棍</label></p>
+    <p><input placeholder="设置 CDKEY，申请 CDKEY 请聊天室发送：凌 CDKEY" v-model="setting.cdkey" style="width:100%" /></p>
+    <p><button @click="save">设置</button></p>
+    <p>{{error}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  data() {
+    return {
+      setting: {
+        enable: true,
+        click: true,
+        cdkey: '',
+      },
+      error: ''
+    }
+  },
+  computed: {
+  },
+  async mounted() {
+    // eslint-disable-next-line no-undef
+    let setting = await $ipc.invoke('fishpi.get.setting');
+    if (setting) this.setting = setting;
+  },
+  methods: {
+    save() {
+      // eslint-disable-next-line no-undef
+      $ipc.send('fishpi.set.setting', this.setting);
+      this.error = '设置成功';
+      setTimeout(() => this.error = '', 2000);
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.layout {
+  padding: 10px;
+  textarea {
+    width: 100%;
+  }
+  button {
+    width: 100%;
+    background: var(--global-active-color);
+    border: none;
+    padding: 10px;
+    color: inherit;
+    cursor: pointer;
+  }
+  .form-flex {
+    display: flex;
+  }
+  .form-input {
+      padding: 5px;
+      height: 30px;
+      flex: 1;
+      line-height: 30px;
+      margin-left: 5px;
+      border: 1px solid var(--global-background-color);
+      &:focus {
+          outline: 0;
+      }
+  }
+}
+</style>
+
